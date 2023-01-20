554851c13781a74efe687c28ae54b22aeed21001
Rowan Martel <92890554+NotEnoughUwU@users.noreply.github.com>
2023-01-19 22:31:46 -0400

    Working on 2 way teleporters

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class Teleport : MonoBehaviour
 {
     public Transform TPPoint;
+    public bool KillZone;
+    bool Teleporting = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == "PlayerCapsule")
         {
+            if (!KillZone)
+            {
+                if (Teleporting)
+                {
+                    Teleporting = false;
+                    return;
+                }
+                Teleporting = true;
+            }
+
+            other.transform.rotation = TPPoint.transform.rotation;
             other.GetComponentInParent<CharacterController>().Move(TPPoint.position - new Vector3(0, 0.906318f, 0) - GameObject.Find("PlayerCapsule").transform.position);
             other.GetComponentInParent<CharacterController>().SimpleMove(Vector3.zero);
         }
